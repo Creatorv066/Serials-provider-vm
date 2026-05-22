287e73575b5c0e495daeef6a4b3d92f1e8c4d08b
ViSHWA <154061854+ViSHWA14510@users.noreply.github.com>
2026-05-22 09:36:49 +0530

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -21,6 +21,6 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You R Banned!.. Contact @creatorv_bots 😝", quote=True)
+            await cmd.reply_text("You R Banned!.. Contact @Marathi_Tv__Serials_bot 😝", quote=True)
             return
     await cmd.continue_propagation()
